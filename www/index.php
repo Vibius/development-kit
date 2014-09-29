75fdbd5ad5791d98efe0d69c267a618219d00c88
Matej Sima <matej.sima@gmail.com>
2014-09-29 17:06:42 +0200

    index.php edited

--- www/index.php
+++ www/index.php
@@ -1,43 +1,9 @@
 <?php
 
-/* BOOTSTRAP */
-
-
-/*require '../vendor/autoload.php';
-
-
-try{
-
-	$aliasManager = new Vibius\Facade\AliasManager();
-
-	$aliasManager->registerAutoloader();
-
-	#$aliasManager->deleteAllAliases();
-
-	
-	Container::open('aliases')->add('aliases', Container::open('aliases'));
-
-	Aliases::add('something',new Vibius\Container\Container);
-	
-	echo something::add('hello','damn it works!');
-
-	Config::addConfig('config');
-	Config::getParameter('debugger','config');
-
-	
-
-
-}catch(Exception $e){
-    //handle errors & exceptions
-    echo "<h1>Exception:</h1>";
-    echo $e->getMessage();
-    echo $e->getLine();
-    echo $e->getFile();
-}
-*/
 $GLOBALS['execution_time'] = microtime(true);
 $framework = require '../system/Framework.php';
 
 define('vibius_INDEXPATH', dirname(__FILE__));
 
 $framework->start();
+
